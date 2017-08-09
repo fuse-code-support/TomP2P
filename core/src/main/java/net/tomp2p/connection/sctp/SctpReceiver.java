ec5ad970693de9cf3e31b120e7d49d7d4236f381
jonaswagner <jonas_wagner99@hotmail.com>
2017-08-09 16:08:40 +0200

    update 6

--- core/src/main/java/net/tomp2p/connection/sctp/SctpReceiver.java
+++ core/src/main/java/net/tomp2p/connection/sctp/SctpReceiver.java
@@ -1,8 +1,11 @@
 package net.tomp2p.connection.sctp;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+
+import net.tomp2p.utils.Pair;
 
 public class SctpReceiver {
 
@@ -22,11 +25,15 @@ public class SctpReceiver {
 			
 			@Override
 			public void onSctpPacket(byte[] data, int sid, int ssn, int tsn, long ppid, int context, int flags) {
+				Pair<InetAddress, Integer> remote = SctpConnectionRegistry.find("" + tsn + sid + ssn);
+				
 				String s = new String(data, StandardCharsets.UTF_8);
 				
 				System.out.println("got message: /n " + s);
 			}
 		};
+		
+		
 		
 		SctpListenThread thread = new SctpListenThread(socket, callback);
 		thread.start();
